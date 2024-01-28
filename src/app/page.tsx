3e34ec030a842778aa5d653001bb2ee6d427a789
usman <usmanthelion@gmail.com>
2024-01-28 13:38:38 +0500

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
    <div className=" container mx-auto my-auto pt-[3rem] sm:px-[10%] md:px-[15%] lg:px-[20%] xl:px-[30%] ">
     <div className="flex  flex-col place-items-center   ">
       
-      <p className=' p-4'>Hey, I'm a Senior Software Engineer at Company. I enjoy working with Next.js and crafting beautiful front-end experiences.</p>
+      <p className=' p-4'>Hey, I`m a Senior Software Engineer at Company. I enjoy working with Next.js and crafting beautiful front-end experiences.</p>
       <p className='p-4'>This portfolio is built with Next.js and a library called Nextra. It allows you to write Markdown and focus on the content of your portfolio.</p>
       <p className='p-4 text-left w-full'>Deploy Your Own</p>
     
