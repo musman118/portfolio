b6365853a01a36e2d5a1cf0a28321161321744f9
musman118 <fa23-bcs-139@cuilahore.edu.pk>
2024-02-27 16:20:29 +0500

    fixed some new issues

--- src/app/components/datafetcher.tsx
+++ src/app/components/datafetcher.tsx
@@ -3,7 +3,7 @@ export default async function datafetcher() {
     try {
         console.log("Making Request");
         
-        const res = await fetch("http://localhost:3000/api",{next:{revalidate:3600}})
+        const res = await fetch("https://musman.xyz/api",{next:{revalidate:3600}})
 
         // if (!res.ok) {
         //     throw new Error('Failed to fetch');
